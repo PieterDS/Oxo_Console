d964e3a0630068db29d864527dba0ec3e91c6c25
Pieter DS <82771801+PieterDS@users.noreply.github.com>
2022-12-03 14:18:44 +0100

    Update Program.cs
    Updated (naming conventions + default access modifiers)

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -16,18 +16,18 @@ namespace OXO
 
             Game.Intro();
 
-            oxoGame.Board.DrawBoard();
+            oxoGame.GameBoard.DrawBoard();
 
-            while (Analyzer.CheckWinning(oxoGame.Board) == false && Analyzer.CheckDraw(oxoGame.Board) == false) 
+            while (Analyzer.CheckWinning(oxoGame.GameBoard) == false && Analyzer.CheckDraw(oxoGame.GameBoard) == false) 
             {
                 oxoGame.Turn();
             }
 
-            if (oxoGame.State.GetValue() == 'X')
+            if (oxoGame.GameState.GetValue() == 'X')
                 Console.WriteLine("Player 1 Wins!");
-            else if (oxoGame.State.GetValue() == 'O')
+            else if (oxoGame.GameState.GetValue() == 'O')
                 Console.WriteLine("Player 2 Wins!");
-            else if (Analyzer.CheckDraw(oxoGame.Board) == true)
+            else if (Analyzer.CheckDraw(oxoGame.GameBoard) == true)
                 Console.WriteLine("It's a Draw!");
             else
                 Console.WriteLine("ERROR 3524");
